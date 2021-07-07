1c2fef44a916a17c661a411ce9819d9a6bc8ecf1
lipwah <56858494+lipwah@users.noreply.github.com>
2021-07-08 01:36:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,9 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 
 #第三方插件
 rm -rf package/lean/luci-theme-argon
