5dd83936cb0c6c0273b02746c21a3e8a0bc49dba
lipwah <56858494+lipwah@users.noreply.github.com>
2021-07-08 08:25:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,12 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+#第三方插件
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon      #argon主题
+git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config         #argon设置
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan                 #微信服务
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
@@ -18,9 +24,3 @@
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
-
-#第三方插件
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon      #argon主题
-git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config         #argon设置
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan                 #微信服务
