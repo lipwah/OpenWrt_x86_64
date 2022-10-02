85b4f07210869d72c17fc3f43ddbaa65359f12c4
lipwah <56858494+lipwah@users.noreply.github.com>
2022-10-02 23:17:18 +0900

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-a
 git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
 
 # Uncomment a feed source
-sed -i "/helloworld/d" "feeds.conf.default"
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
